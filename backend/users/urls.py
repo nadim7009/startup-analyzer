88e0a55470fb42ecadcd676bd83675d07c35e0ae
Nadim Mahmud <151434889+nadim7009@users.noreply.github.com>
2026-03-29 22:40:51 +0600

    Delete backend directory

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, login, forgot_password, reset_password
-
-urlpatterns = [
-    path("register/", register, name="register"),
-    path("login/", login, name="login"),
-    path("forgot-password/", forgot_password, name="forgot_password"),
-    path("reset-password/", reset_password, name="reset_password"),
-]
